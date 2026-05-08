0c889409b548e49bba4ebeb4f34bcd3539aa694a
kondabathul405 <kondabathul405@users.noreply.github.com>
2026-05-08 20:12:38 +0530

    Initial commit: Add LearnPlaywrightBatch2x project files

---
+++ chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,12 @@
+var $ = 10;
+var _a = 19;
+var p = 10;
+
+var ab123 = 24;
+
+var Name = "pramod";
+var name = "mani"
+
+var pramod_dutta = "Hello";
+var pramod$dutta = "hello";
+var pramod1234 = "hello";
